e52673c6eff17b5ff3f87f11bd04572a07ce896b
liquidz <liquidz.uo@gmail.com>
2023-09-02 07:44:00 +0900

    feat: Add string utility

---
+++ src/uniform/string.clj
@@ -0,0 +1,9 @@
+(ns uniform.string)
+
+(defn width
+  [s]
+  (reduce
+    (fn [accm c]
+      (+ accm
+         (if (= 1 (count (.getBytes (str c)))) 1 2)))
+    0 s))
---
+++ test/uniform/string_test.clj
@@ -0,0 +1,11 @@
+(ns uniform.string-test
+  (:require
+    [clojure.test :as t]
+    [uniform.string :as sut]))
+
+(t/deftest width-test
+  (t/is (= 0  (sut/width "")))
+  (t/is (= 15 (sut/width "hello lispworld")))
+  (t/is (= 15 (sut/width "こんにちは 世界")))
+  (t/is (= 15 (sut/width "helloこんにちは")))
+  (t/is (= 15 (sut/width "surrogatepair𩸽"))))
