bfb96021f8a90fb8fed272a04dc5c09930b86304
liquidz <liquidz.uo@gmail.com>
2023-09-10 08:51:55 +0900

    feat: Update format-code to apply ns-missing-linebreaks

--- src/uniform/core.clj
+++ src/uniform/core.clj
@@ -2,22 +2,29 @@
   (:require
     [rewrite-clj.node :as r.node]
     [rewrite-clj.parser :as r.parser]
-    [uniform.rule :as u.rule]))
+    [rewrite-clj.zip :as r.zip]
+    [uniform.rule :as u.rule]
+    [uniform.rule.ns :as u.r.ns]))
 
 (defn format-code
   [code]
   (-> code
       (r.parser/parse-string-all)
-      (u.rule/apply-rules [u.rule/missing-whitespace
-                           u.rule/hard-tab-to-space
-                           u.rule/too-many-spaces
-                           u.rule/too-many-linebreaks
-                           u.rule/comma])
-      (u.rule/apply-rules [u.rule/indent])
+      (u.rule/apply-rules
+        [u.rule/missing-whitespace
+         u.rule/hard-tab-to-space
+         u.rule/too-many-spaces
+         u.rule/too-many-linebreaks
+         u.rule/comma])
+      (u.rule/apply-rules
+        #(r.zip/find-value % r.zip/next 'ns)
+        [u.r.ns/ns-missing-linebreaks])
+      (u.rule/apply-rules
+        [u.rule/indent])
       (r.node/string)))
 
 (comment
-  (let [org  "(   foo , (bar(baz)  )\n\n      baz)
+  (let [org  "(ns (:require [foo] [bar]))\n(   foo , (bar(baz)  )\n\n      baz)
              {:foo   \"  bar\"}"]
     (println org)
     (println "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv")
